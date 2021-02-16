a6ed6e4dbc9de665198105660a977d823561622a
Roger Spurrell <rspurrell@hotmail.com>
2021-02-16 04:47:34 -0500

    Added Sidewinder packet polling

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,47 @@
-/* Teensyduino Core Library
- * http://www.pjrc.com/teensy/
- * Copyright (c) 2017 PJRC.COM, LLC.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * 1. The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * 2. If the Software is incorporated into a build system that allows
- * selection among a list of target devices, then similar target
- * devices manufactured by PJRC.COM must be included in the list of
- * target devices and selectable in the same manner.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+#include "sidewinder.h"
 
-#include <Arduino.h>
+#define PIN_SW_CLOCK CORE_RXD1_PIN
+#define PIN_SW_DATA CORE_RXD0_PIN
+#define PIN_SW_TRIGGER CORE_TXD1_PIN
 
 extern "C" int main(void)
 {
 #ifdef USING_MAKEFILE
 
-	// To use Teensy without Arduino IDE, simply put your code here.
-	// For example:
+#ifdef DEBUG
+    while (!Serial || !Serial.availableForWrite());
+    Serial.begin(9600);
+    Serial.println("Debug serial connected");
+#endif
 
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(1000);
-		digitalWriteFast(13, LOW);
-		delay(500);
-	}
+    Sidewinder sw(PIN_SW_CLOCK, PIN_SW_TRIGGER, PIN_SW_DATA);
 
+    delay(1000);
+    
+    sw_data_t packet;
+    while (1)
+    {
+#ifdef DEBUG
+        elapsedMicros elapsed;
+#endif
+		packet = sw.Poll();
 
+		// validate packet
+		bool isPacketValid = sw.CheckParity(packet);
+#ifdef DEBUG	
+		Serial.printf("Packet valid: %d\r\n", isPacketValid);
+#endif
+
+		// write packet data to usb output to pc
+
+		// delay before next update
+#ifndef DEBUG
+		delay(4);
 #else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
+		Serial.printf("Cycle time: %d\r\n", (uint32_t)elapsed);
+#endif
+    }
+
 #endif
 }
 
---
+++ src/sidewinder.h
@@ -0,0 +1,72 @@
+#ifndef TeensySidewinder
+#define TeensySidewinder
+
+#include <Arduino.h>
+
+#define PACKET_DATA_BYTES_COUNT 6
+#define TIMEOUT 1000 // 1 millisecond
+#define TRIGGER_HOLD 600 // microseconds
+
+typedef struct
+{
+	// the ints are used to access the struct-data at bit-level
+	uint8_t volatile bytes[6];
+	
+	// bit  1
+	const uint8_t Trigger() const { return bytes[0] & 1; }
+	// bit  2
+	const uint8_t Top() const { return bytes[0] >> 1 & 1; }
+	// bit  3
+	const uint8_t Up() const { return bytes[0] >> 2 & 1; }
+	// bit  4
+	const uint8_t Down() const { return bytes[0] >> 3 & 1; }
+
+	// bit  5
+	const uint8_t A() const { return bytes[0] >> 4 & 1; }
+	// bit  6
+	const uint8_t B() const { return bytes[0] >> 5 & 1; }
+	// bit  7
+	const uint8_t C() const { return bytes[0] >> 6 & 1; }
+	// bit  8
+	const uint8_t D() const { return bytes[0] >> 7 & 1; }
+	
+	// bit  9
+	const uint8_t Shift() const { return bytes[1] & 1; }
+
+	// bits 10-19
+	const uint16_t XAxis() const { return ((bytes[2] & 7) << 8 | (bytes[1] & 254)) >> 1; }
+	// bits 20-29
+	const uint16_t YAxis() const { return ((bytes[3] & 31) << 8 | (bytes[2] & 248)) >> 3; }
+	// bits 30-36
+	const uint8_t Throttle() const { return ((bytes[4] & 15) << 8 | (bytes[3] & 224)) >> 5; }
+	// bits 37-42
+	const uint8_t Rotation() const { return ((bytes[5] & 3) << 8 | (bytes[4] & 240)) >> 4; }
+
+	// bits 43-46
+	const uint8_t Head() const { return bytes[5] >> 2 & 15; }
+
+	// bit 47
+	const uint8_t Reserved() const { return bytes[5] >> 6 & 1; }
+	// bit 48
+	const bool Parity() const { return bytes[5] >> 7 & 1; }
+} sw_data_t;
+
+class Sidewinder
+{
+private:
+    static const sw_data_t SW_DATA_EMPTY;
+    uint8_t pin_clock, pin_trigger, pin_data;
+
+    #ifdef DEBUG
+    void debug(sw_data_t p, uint32_t bytesRead, uint32_t elapsed);
+    #endif
+
+public:
+    Sidewinder(uint8_t pinClock, uint8_t pinTrigger, uint8_t pinData);
+    ~Sidewinder();
+
+    sw_data_t Poll();
+    bool CheckParity(sw_data_t p);
+};
+
+#endif
