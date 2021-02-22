bebb5739b54568909da95122ab105dbda67d6281
Roger Spurrell <rspurrell@hotmail.com>
2021-02-21 21:21:05 -0500

    Added Sidewinder packet to Teensy joystick mapping

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ extern "C" int main(void)
     Serial.println("Debug serial connected");
 #endif
 
+    Joystick.useManualSend(true);
+
     Sidewinder sw(PIN_SW_CLOCK, PIN_SW_TRIGGER, PIN_SW_DATA);
 
     delay(1000);
@@ -24,6 +26,8 @@ extern "C" int main(void)
 #ifdef DEBUG
         elapsedMicros elapsed;
 #endif
+
+        // retrieve sidewinder packet
 		packet = sw.Poll();
 
 		// validate packet
@@ -31,8 +35,44 @@ extern "C" int main(void)
 #ifdef DEBUG	
 		Serial.printf("Packet valid: %d\r\n", isPacketValid);
 #endif
+        if (!isPacketValid)
+        {
+            continue; // packet parity check failed
+        }
 
-		// write packet data to usb output to pc
+        // axis
+        Joystick.X(packet.XAxis());
+        Joystick.Y(packet.YAxis());
+        Joystick.Zrotate(((packet.Rotation() + 1) << 4) - 1);
+        Joystick.slider(((packet.Throttle() + 1) << 3) - 1);
+
+        // hat
+        int16_t angle = -1;
+        if (packet.Head() > 0)
+        {
+            uint8_t h = packet.Head();
+            if (h == 1)
+                h = 9;
+            h--;
+            angle = h * 45;
+        }
+        Joystick.hat(angle);
+
+        // buttons
+        uint8_t shift = !packet.Shift() << 3;
+        uint8_t btns = packet.bytes[0];
+        for (size_t i = 0; i < 8; i++)
+        {
+            // clear current button value
+            Joystick.button(i + 1, 0);
+            Joystick.button((i | 8) + 1, 0);
+
+            // set new button value
+            Joystick.button((i | shift) + 1, !(btns >> i & 1));
+        }
+
+        // send packet data to usb output to pc
+        Joystick.send_now();
 
 		// delay before next update
 #ifndef DEBUG
